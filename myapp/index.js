414b3030144e3adfefcd9bb00e0bb3850dd3c0ac
kixv <294199673@qq.com>
2019-03-13 15:46:37 +0800

    test

--- myapp/index.js
+++
@@ -1,9 +0,0 @@
-let express= require("express");
-let http = require("http");
-let app=express();
-
-app.use((req,res)=>{
-	res.writeHead("200",{"content-type":"text/plain"});
-	res.end("h w");
-});
-http.createServer(app).listen(8070);
